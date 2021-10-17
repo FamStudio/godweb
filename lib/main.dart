e7fec3309f1eb30c4389a27fdb5e45ab6d2060d3
FaNR1de <88655607+FamStudio@users.noreply.github.com>
2021-10-17 11:24:49 +0700

    firebase add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:godweb/Screens/landing.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() => runApp(ChordsApp());
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ChordsApp());
+}
 
 class ChordsApp extends StatelessWidget{
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context)
   {
@@ -14,7 +23,18 @@ class ChordsApp extends StatelessWidget{
             primaryColor: Color.fromRGBO(50, 65, 85, 1),
             textTheme: TextTheme(headline6: TextStyle(color: Colors.white))
         ),
-        home: LandingPage()
+        home: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot)
+          {if(snapshot.hasError){
+          print("Error");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return LandingPage();
+          }
+          return CircularProgressIndicator();
+          }
+        )
     );
   }
 }
